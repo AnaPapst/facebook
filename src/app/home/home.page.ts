bdf4a0f7bcf57f0f96448a014db0e88d00452706
Ana Papst <ana.laura@helpper.com.br>
2019-08-13 11:42:57 -0300

    Facebook

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  user:any = {};
+  
+  constructor(public navCtrl: NavController, private fb: Facebook) {}
+  loginFb(){
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then((res: FacebookLoginResponse) => {
+        if(res.status === 'connected'){
+          this.user.img = "https://graph.facebook.com/" + res.authResponse.userID + "/picture?type=square";
+          
+        } else {
+          alert('Login failed');
+        }
+        console.log('Logged into Facebook!', res)
+      })
+      .catch(e => console.log('Error logging into Facebook', e));
+  }
 }
